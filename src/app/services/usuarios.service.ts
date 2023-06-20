a9d6317f0cfcd5875571f4b78275d968cb3d6459
rogerio <rogeriosilva60@gmail.com>
2023-06-20 16:34:38 -0300

    Added new tweaks for deploy

--- src/app/services/usuarios.service.ts
+++ src/app/services/usuarios.service.ts
@@ -1,30 +1,33 @@
 import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
+import { environment } from "../../environments/environment";
 
 @Injectable({
     providedIn: 'root'
 })
 export class UsuarioService {
 
+  baseUrl = environment.apiUrl;
+
   constructor(private http: HttpClient) {}
 
   buscarAgrupadoPorEstado() {
-    return this.http.get("http://127.0.0.1:8080/v1/usuarios/agrupado-por-estado");
+    return this.http.get(`${this.baseUrl}/v1/usuarios/agrupado-por-estado`);
   }
 
   buscarImcMedioPorFaixaEtariadeDezEmDezAnos() {
-    return this.http.get("http://127.0.0.1:8080/v1/usuarios/imc-faixa-etaria-dez-em-dez-anos");
+    return this.http.get(`${this.baseUrl}/v1/usuarios/imc-faixa-etaria-dez-em-dez-anos`);
   }
 
   buscarPercentualDeObesosPorGenero() {
-    return this,this.http.get("http://127.0.0.1:8080/v1/usuarios/percentual-obeso-por-genero");
+    return this,this.http.get(`${this.baseUrl}/v1/usuarios/percentual-obeso-por-genero`);
   }
 
   buscarMediaIdadeParaCadaGrupoSanguineo() {
-    return this.http.get("http://127.0.0.1:8080/v1/usuarios/media-idade-grupo-sanguineo");
+    return this.http.get(`${this.baseUrl}/v1/usuarios/media-idade-grupo-sanguineo`);
   }
 
   buscarQuantidadePossiveisDeDoadoresParaCadaGrupo() {
-    return this.http.get("http://127.0.0.1:8080/v1/usuarios/possiveis-doadores-por-grupo-sanguineo");
+    return this.http.get(`${this.baseUrl}/v1/usuarios/possiveis-doadores-por-grupo-sanguineo`);
   }
 }
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+    production: true,
+    apiUrl: 'https://desafio-abs-api-ad417057eb6f.herokuapp.com'
+}
